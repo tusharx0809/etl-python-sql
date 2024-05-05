9a9d179c117b80f471ee67c75795a64e7a0099e9
tusharx0809 <tusharbhatia0809@outlook.com>
2024-05-04 22:28:15 -0400

    csv-reading

--- first-etl.py
+++ first-etl.py
@@ -1,1 +1,9 @@
 """This is a small ETL project which extracts data from csv and loads into the MSSQL server"""
+import os
+import pandas as pd
+
+file = 'D:\\Python\\etl-python-sql\\sample.csv'
+
+df = pd.read_csv(file, encoding='latin1')
+
+print(df.head())
