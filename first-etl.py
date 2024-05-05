1ae8338e4ed50796254e9067ffcae16f4fcc412d
tusharx0809 <tusharbhatia0809@outlook.com>
2024-05-04 22:52:51 -0400

    data-cleaned

--- first-etl.py
+++ first-etl.py
@@ -1,9 +1,25 @@
 """This is a small ETL project which extracts data from csv and loads into the MSSQL server"""
-import os
+#The csv contains random data with 5000 rows, out of which we are going to extract only top 100 rows with valiues Name, Province and Profession. Then we will load these values in our MS-SQL server
+
+
 import pandas as pd
 
 file = 'D:\\Python\\etl-python-sql\\sample.csv'
+columns = ['column' + str(i) if i != 0 else '' for i in range(1, 11)]
+df = pd.read_csv(file, encoding='latin1', names=columns)
 
-df = pd.read_csv(file, encoding='latin1')
+to_sql = [] # we will use this array to load data into our SQL server
 
-print(df.head())
+i = 0 # we will increment this to 100 so we can get a total of 100 values
+for idx,row in df.iterrows():
+    temp = []
+    temp.append(row['column3']) # append Name in temp array
+    temp.append(row['column8']) # append Province in temp array
+    temp.append(row['column9']) # append Profession in temp array
+    to_sql.append(temp) # append temp array to to_sql in each loop
+    i += 1
+    if i == 100:
+        break
+print(len(to_sql))   
+
+    
