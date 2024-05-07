d895935e42a47eab3b54e286b61a3ce8d24dde88
tusharx0809 <tusharbhatia0809@outlook.com>
2024-05-07 12:24:43 -0400

    minor-changes

---
+++ project/load.py
@@ -0,0 +1,25 @@
+import random
+from debugpy import connect
+import pyodbc
+import pandas as pd
+
+#create the connection string with MS-SQL server information, Here are we are going to load data to testing2 database which will be our target
+connection_string_source = f"""
+    Driver=ODBC Driver 17 for SQL Server;
+    Server=localhost\SQLEXPRESS;
+    Database=testing2;
+    Trusted_Connection=yes;
+"""
+
+#create the connection
+connection = pyodbc.connect(connection_string_source)
+
+cursor = connection.cursor()
+
+create_query ="""
+create table target_table(
+customer_id int,
+full_name varchar(255)
+)"""
+cursor.execute(create_query)
+connection.commit()
